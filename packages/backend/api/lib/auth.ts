f4b5363bfb426238c92948a00d7d8d2dfb03bc20
Max Gerber <max@stytch.com>
2025-09-03 11:01:36 -0700

    checkin: Split Consumer app

---
+++ packages/backend/api/lib/auth.ts
@@ -0,0 +1,78 @@
+import {createRemoteJWKSet, jwtVerify} from "jose";
+import {createMiddleware} from "hono/factory";
+import {HTTPException} from "hono/http-exception";
+import {getCookie} from "hono/cookie";
+
+
+let jwks: ReturnType<typeof createRemoteJWKSet> | null = null;
+
+async function validateStytchJWT(token: string, env: Env) {
+    if (!jwks) {
+        jwks = createRemoteJWKSet(new URL(`${env.STYTCH_DOMAIN}/.well-known/jwks.json`))
+    }
+
+    return await jwtVerify(token, jwks, {
+        audience: env.STYTCH_PROJECT_ID,
+        issuer: [env.STYTCH_DOMAIN],
+        typ: "JWT",
+        algorithms: ['RS256'],
+    })
+}
+
+
+/**
+ * stytchAuthMiddleware is a Hono middleware that validates that the user is logged in
+ * It checks for the stytch_session_jwt cookie set by the Stytch FE SDK
+ */
+export const stytchSessionAuthMiddleware = createMiddleware<{
+    Variables: {
+        userID: string
+    },
+    Bindings: Env,
+}>(async (c, next) => {
+    const sessionCookie = getCookie(c, 'stytch_session_jwt');
+
+    try {
+        const verifyResult = await validateStytchJWT(sessionCookie ?? '', c.env)
+        c.set('userID', verifyResult.payload.sub!);
+    } catch (error) {
+        console.error(error);
+        throw new HTTPException(401, {message: 'Unauthenticated'})
+    }
+
+    await next()
+})
+
+/**
+ * stytchBearerTokenAuthMiddleware is a Hono middleware that validates that the request has a Stytch-issued bearer token
+ * Tokens are issued to clients at the end of a successful OAuth flow
+ */
+export const stytchBearerTokenAuthMiddleware = createMiddleware<{
+    Bindings: Env,
+}>(async (c, next) => {
+    const authHeader = c.req.header('Authorization')
+    const url = new URL(c.req.url);
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        const wwwAuthValue = `Bearer error="Unauthorized", error_description="Unauthorized", resource_metadata="${url.origin}/.well-known/oauth-protected-resource"`;
+        const responseHeaders = new Headers();
+        
+        responseHeaders.set('WWW-Authenticate', wwwAuthValue);
+        const res = new Response(null, {status: 401, headers: responseHeaders})
+        throw new HTTPException(401, {message: 'Missing or invalid access token', res: res})
+    }
+    const accessToken = authHeader.substring(7);
+
+    try {
+        const verifyResult = await validateStytchJWT(accessToken, c.env);
+        (c.executionCtx as any).props = {
+            claims: verifyResult.payload,
+            accessToken,
+        }
+    } catch (error) {
+        console.error(error);
+        throw new HTTPException(401, {message: 'Unauthenticated'})
+    }
+
+    await next()
+})
---
+++ packages/backend/types.d.ts
@@ -0,0 +1,24 @@
+export type Todo = {
+    id: string;
+    text: string;
+    completed: boolean;
+}
+
+// Context from the auth process, extracted from the Stytch auth token JWT
+// and provided to the MCP Server as this.props
+export type AuthenticationContext = {
+    claims: {
+        "iss": string,
+        "scope": string,
+        "sub": string,
+        "aud": string[],
+        "client_id": string,
+        "exp": number,
+        "iat": number,
+        "nbf": number,
+        "jti": string,
+    },
+    accessToken: string
+}
+
+export {};
