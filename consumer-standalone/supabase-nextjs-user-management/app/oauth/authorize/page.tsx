df3f41ed2845fa45c254ef37df82f069e257b88c
Vanshaj Singhania <vsinghania@stytch.com>
2025-09-12 14:03:11 -0700

    feat: use `IdentityProvider` to attest trusted auth tokens (#22)
    * use IdentityProvider to attest trusted auth tokens
    
    * prettier

--- consumer-standalone/supabase-nextjs-user-management/app/oauth/authorize/page.tsx
+++ consumer-standalone/supabase-nextjs-user-management/app/oauth/authorize/page.tsx
@@ -3,15 +3,12 @@
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { IdentityProvider } from '@stytch/nextjs';
-import { createStytchUIClient } from '@stytch/nextjs/dist/index.ui';
 import StytchProvider from '@/components/StytchProvider';
 import { createClient } from '@/utils/supabase/client';
 
-const stytch = createStytchUIClient(process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN!);
-
 export default function AuthenticatePage() {
   const router = useRouter();
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+  const [token, setToken] = useState<string | null>(null);
 
   useEffect(() => {
     const checkAuthentication = async () => {
@@ -28,24 +25,13 @@ export default function AuthenticatePage() {
         return;
       }
 
-      // if we've already attested this supabase session with stytch,
-      // we must skip this step
-      const hasStytchSession = stytch.session.getInfo().session;
-      if (!hasStytchSession && token) {
-        await stytch.session.attest({
-          profile_id: process.env.NEXT_PUBLIC_STYTCH_TOKEN_PROFILE!,
-          token,
-          session_duration_minutes: 60,
-        });
-      }
-
-      setIsAuthenticated(true);
+      setToken(token);
     };
 
     checkAuthentication();
   }, [router]);
 
-  if (!isAuthenticated) {
+  if (!token) {
     return null;
   }
 
@@ -55,7 +41,12 @@ export default function AuthenticatePage() {
         <div className="row">
           <div className="col-12">
             <h1 className="header">Stytch OAuth Authorization</h1>
-            <IdentityProvider />
+            <IdentityProvider
+              authTokenParams={{
+                trustedAuthToken: token!,
+                tokenProfileID: process.env.NEXT_PUBLIC_STYTCH_TOKEN_PROFILE!,
+              }}
+            />
           </div>
         </div>
       </div>
